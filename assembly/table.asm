d0ca1a4e4d250f22d04f76acb7ecfc1fb2d05d5c
Tommy <tommyy94@users.noreply.github.com>
2018-05-05 18:53:21 +0000

    Update table.asm

--- assembly/table.asm
+++ assembly/table.asm
@@ -6,14 +6,14 @@
 
 reset:
     ;init stack pointer
-	ldi r16, high(RAMEND)
-	out SPH, r16
-	ldi r16, low(RAMEND)
-	out SPL, r16
+    ldi r16, high(RAMEND)
+    out SPH, r16
+    ldi r16, low(RAMEND)
+    out SPL, r16
     
     ;load address to Z register
-	ldi ZH, high(2*data) ;program memory is stored in 16-bit words, so
-	ldi ZL, low(2*data)  ;multiply by 2 to extract bytes
+    ldi ZH, high(2*data) ;program memory is stored in 16-bit words, so
+    ldi ZL, low(2*data)  ;multiply by 2 to extract bytes
 
     ldi r17, TABLE_SIZE ;load counter
     
@@ -23,7 +23,7 @@ load:
     brne load ;if r17 != 0, goto load
 
 end:
-	rjmp end
+    rjmp end
 
 
 data: .db "Generic text", "XD"
